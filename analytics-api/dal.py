0ab5269294df434f21c20be07156f43fb48d99ac
david <mdavidl@gmail.com>
2026-02-12 12:46:00 +0200

    finish the sql querys

--- analytics-api/dal.py
+++ analytics-api/dal.py
@@ -1,23 +1,48 @@
+from consumer.mysql_connection import Connection
+
+connection = Connection()
+
+
+def select_many(query: str):
+    conn = connection.get_connection()
+    with conn.cursor(dictionary=True) as cursor:
+        cursor.execute(f"USE {connection.database}")
+        cursor.execute(query)
+        result = cursor.fetchall()
+    return result
+
+
 def get_10_client_with_max_orders():
     query = """
-    SELECT customers.customerName,
-    COUNT(order.orderNumber) 
-    FROM customers
-     
-            """
-    return query
+    SELECT c.customerNumber, c.customerName, COUNT(o.orderNumber) as count
+    FROM customers AS c INNER JOIN orders o
+    ON c.customerNumber = o.customerNumber
+    GROUP BY c.customerNumber, c.customerName
+    ORDER BY count
+    LIMIT 10
+           """
+    return select_many(query)
 
 def get_client_withnt_ordres():
     query = """
-    SELECT customers.customerName FROM customers
-    INNER JOIN order.customerNumber
-    ON order.customerNumber = customers.customerNumber
-    WHERE COUNT(order.orderNumber) < 1
+    SELECT c.customerNumber, c.customerName, COUNT(o.orderNumber) as count
+    FROM customers AS c LEFT JOIN orders o
+    ON c.customerNumber = o.customerNumber
+    GROUP BY c.customerNumber, c.customerName
+    HAVING COUNT(o.orderNumber) = 0
             """
-    return query
+    return select_many(query)
 
 def get_client_withnt_credit_limit():
     query = """
-    SELECT customers.customerName FROM customers
+    SELECT c.customerNumber, c.customerName, c.creditLimit
+        FROM customers  AS c INNER JOIN orders o
+        ON c.customerNumber = o.customerNumber
+        WHERE c.creditLimit = 0.0
+        GROUP BY c.customerNumber, c.customerName
             """
-    return query
+    return select_many(query)
+
+
+
+
